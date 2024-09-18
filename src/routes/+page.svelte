9a46bd3010fc85e793ea706e7dc7a2baaac5625f
Asare Gideon <asaregid506@gmail.com>
2024-09-18 17:59:33 +0000

    team page changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,7 +11,7 @@
 
 <svelte:head>
   <title>Home</title>
-  <meta name="description" content="Svelte demo app" />
+  <meta name="description" content="Bison Imperial" />
 </svelte:head>
 
 <section class="index-page">
